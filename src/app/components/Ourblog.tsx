86c29cdb45a61b7fb40f126eaa76a9764c0f4a7d
Saba Muhammad Riaz <sabaranicool94@gmail.com>
2024-12-09 10:48:19 -0800

    Add files via upload

---
+++ src/app/components/Ourblog.tsx
@@ -0,0 +1,129 @@
+import Image from "next/image"
+import image1 from"../../../Gallery/Group 53.png"
+import image2 from "../../../Gallery/Group 48.png"
+import image3 from "../../../Gallery/Group 49.png"
+import image4 from "../../../Gallery/Group 50.png"
+import image5 from"../../../Gallery/Group 47.png"
+
+
+export default function Blog(){
+    return(
+        <><div className="w-4/6 h-[260px] p-4 overflow-hidden flex justify-center items-center mx-auto aspect-w-16 aspect-h-8">
+            <Image
+                src={image1}
+                width={400}
+                height={200}
+                alt="pic" />
+        </div><div className="font-sans p-4 mx-auto lg:max-w-6xl md:max-w-4xl">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-16">
+
+                    <div className="bg-white overflow-hidden cursor-pointer">
+                        <div className="w-full h-[150px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                        <Image 
+                        src={image2}
+                        width={400}
+                        height={200}
+                        alt="pic"/>                        </div>
+
+<div className="mt-6 text-center">
+                            <h3 className="text-small font-medium text-gray-800">Going all in with millinial design</h3>
+                            <button className="flex mx-auto mt-6 text-black border-b-2 text-border-underline-black   underline-bg-black hover:border-indigo-500 ">
+      Read more
+    </button>                      </div>
+
+                        <div className="flex justify-center space-x-1 mt-3">
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                        </div>
+                    </div>
+
+                    <div className="bg-white overflow-hidden cursor-pointer">
+                        <div className="w-full h-[150px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                        <Image 
+                        src={image3}
+                        width={400}
+                        height={200}
+                        alt="pic"/>                         </div>
+
+<div className="mt-6 text-center">
+                            <h3 className="text-small font-medium text-gray-800">Going all in with millinial design</h3>
+                            <button className="flex mx-auto mt-6 text-black border-b-2 text-border-underline-black   underline-bg-black hover:border-indigo-500 ">
+      Read more
+    </button>                      </div>
+                        <div className="flex justify-center space-x-1 mt-3">
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#facc15]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                            <svg className="w-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                            </svg>
+                        </div>
+                    </div>
+
+                    <div className="bg-white overflow-hidden cursor-pointer">
+                        <div className="w-full h-[150px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                        <Image 
+                        src={image4}
+                        width={600}
+                        height={400}
+                        alt="pic"/>                                 
+                        </div>
+
+                        <div className="mt-6 text-center">
+                            <h3 className="text-small font-medium text-gray-800">Going all in with millinial design</h3>
+                            <button className="flex mx-auto mt-6 text-black border-b-2 text-border-underline-black   underline-bg-black hover:border-indigo-500 ">
+      Read more
+    </button>                      </div>
+    </div>
+    </div>  
+                    </div>
+
+                    <div className="bg-white overflow-hidden cursor-pointer">
+                        </div>
+                        <div className="bg-white overflow-hidden cursor-pointer">
+                        <Image 
+                        src={image5}
+                        width={1400}
+                        height={600}
+                        alt="pic"/>         <br></br>               </div><br></br>
+                        
+                   
+
+
+                   
+          
+
+            </>)}
+ 
---
+++ src/app/components/hero2.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import Sofa from "../../../Gallery/Rocket single seater 1.png"
+import chair from"../../../Gallery/WhatsApp Image 2024-12-07 at 10.49.27 PM.jpeg"
+import sofa2 from"../../../Gallery/WhatsApp Image 2024-12-07 at 10.49.45 PM.jpeg"
+export default function Hero(){
+    return(
+        <section className="bg-[#FBEBB5] min-h-screen flex items-center px-4 md:px-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
+                <div className="flec flex-col justify-center space-y-4">
+                    <h1 className="text-4xl md:text-6xl font-bold text-gray-800 leading=snug">
+                        Roacket single <br/> seater
+                    </h1>
+                    <p className="text-lg text-gray-600">
+                        Elegant and comfortable seating for you modern living room.
+                    </p>
+                    <a
+                    href="#shop"
+                    className="inline-block text-black font-medium underline hover:no-underline hover:text-gray-800 transition">
+                        Shop Now
+                    </a>
+                    </div>
+<div className="flex justify-center items-center">
+    <Image 
+    src={Sofa}
+    width={1200}
+    height={1200}
+    alt="rocket single sofa"
+    className="w-full max-w-sm md-w-md object--contain:max/"></Image>
+</div></div></section>
+)}
